86088856d84c98fb92b460c40f775766a8f37ef3
Waxby <waxby777@gmail.com>
2026-04-16 15:08:52 +0300

    _fetch_one_version renamed to fetch_one

--- main/vanilla_fetcher.py
+++ main/vanilla_fetcher.py
@@ -2,23 +2,51 @@ import logging
 from typing import Optional
 import asyncio
 
-from main.base_fetcher import BaseFetcher
+from base_fetcher import BaseFetcher
 
 URL = "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json"
+logging.basicConfig(level=logging.INFO)
 
 class VanillaFetcher(BaseFetcher):
     platform_id = 'vanilla'
     platform_name = "Vanilla"
     platform_uid = 'net.minecraft'
 
-    async def fetch_mc_versions(self) -> Optional[dict]:
+    async def fetch(self) -> Optional[dict]:
         data = await self.get_json(URL)
         if not data:
             return None
         
         versions = []
+        async def fetch_one(v: dict) -> Optional[dict]:
+            ver_data = await self.get_json(v["url"])
+            if not ver_data:
+                return None
+        
+            downloads = ver_data.get("downloads", {})
+            if "server" not in downloads:
+                return None
+        
+            server = downloads["server"]
+            logging.info(f"Fetched {v["id"]} Minecraft version(Vanilla)")
+            return {
+                "mcVersion": v["id"],
+                "builds": [
+                    {
+                        "build": None,
+                        "type": v["type"],
+                        "releaseTime": v["releaseTime"],
+                        "recommended": v["type"] == "release",
+                        "download": {
+                            "name": f"minecraft_server.{v['id']}.jar",
+                            "url": server["url"],
+                            "sha1": server["sha1"]
+                        }
+                    }
+                ]
+            }
 
-        tasks = [self._fetch_one_version(v) for v in data["versions"]]
+        tasks = [fetch_one(v) for v in data["versions"]]
         results = await asyncio.gather(*tasks)
         for entry in results:
             if entry:
@@ -26,35 +54,8 @@ class VanillaFetcher(BaseFetcher):
         
         logging.info('Added Minecraft versions to JSON(Vanilla)')
         return {
-            "formatVersion": 1,
-            "platform":      "vanilla",
-            "uid":           self.platform_uid,
-            "versions":      versions
-        }
-
-    async def fetch_builds(self) -> list:
-        logging.warning('Vanilla does not support builds')
-        return []
-
-    async def _fetch_one_version(self, v: dict) -> Optional[dict]:
-        ver_data = await self.get_json(v["url"])
-        if not ver_data:
-            return None
-        
-        downloads = ver_data.get("downloads", {})
-        if "server" not in downloads:
-            return None
-        
-        server = downloads["server"]
-        return {
-            "mcVersion": v["id"],
-            "build": None,
-            "type": v["type"],
-            "releaseTime": v["releaseTime"],
-            "recommended": v["type"] == "release",
-            "download": {
-                "name": f"minecraft_server.{v['id']}.jar",
-                "url": server["url"],
-                "sha1": server["sha1"]
-            }
+            "platform": "vanilla",
+            "name": "Vanilla",
+            "uid": self.platform_uid,
+            "versions": versions
         }
